09e7073119af609430133cd31e781699caa91467
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2020-07-19 12:13:19 +0300

    Mark convertToJSON func with extern C

--- include/c_interface.cpp
+++ include/c_interface.cpp
@@ -1,5 +1,5 @@
 #include "xml2json.hpp"
 
-const char * convertToJSON(const char *xml_str) {
+extern "C" __attribute__((visibility("default"))) const char * convertToJSON(const char *xml_str) {
     return xml2json(xml_str).c_str();
 }
