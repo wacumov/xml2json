43bc4f0b14d296b1e605b409e6169fbac3010137
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2020-07-19 11:17:36 +0300

    Implement Swift wrapper

--- Sources/xml2json/XMLToJSON.swift
+++ Sources/xml2json/XMLToJSON.swift
@@ -2,5 +2,13 @@ import xml2json_cpp
 
 public struct XMLToJSON {
     
-    
+    public static func convert(_ xmlString: String) -> String? {
+        let jsonString = xmlString.withCString {
+            convertToJSON($0)
+        }
+        guard let cString = jsonString else {
+            return nil
+        }
+        return String(cString: cString)
+    }
 }
